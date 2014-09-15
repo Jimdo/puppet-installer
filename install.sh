38bd61fe24012df3e8c932df0579115e69de28ec
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-09-15 22:29:12 +0200

    Install latest Puppet version if -v is missing

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #/ Install a specific version of Puppet
-#/ Usage: install.sh -v <Puppet version>
+#/ Usage: install.sh [-v <Puppet version>]
 
 set -e
 
-VERSION=
+version=
 
 while test "$#" -ne 0; do
     case "$1" in
     -h|--h|--he|--hel|--help)
         grep '^#/' <"$0" | cut -c4-; exit 0 ;;
     -v|--v|--ve|--ver|--vers|--versi|--versio|--version)
-        VERSION=$2; shift 2 ;;
+        version=$2; shift 2 ;;
     --)
         shift; break ;;
     -|[!-]*)
@@ -21,23 +21,24 @@ while test "$#" -ne 0; do
     esac
 done
 
-# TODO: make version optional
-test -n "$VERSION" || { echo >&2 "error: version required"; exit 1; }
-
-DISTRO=$(lsb_release -cs)
-PACKAGE="puppetlabs-release-$DISTRO.deb"
-
-CURRENT=$(apt-cache policy puppet | awk '/Installed:/ {print $2}' 2>/dev/null)
-if test "$CURRENT" = "$VERSION"; then
-    echo "Puppet version $VERSION already installed."
+current=$(apt-cache policy puppet | awk '/Installed:/ {print $2}' 2>/dev/null)
+if test "$current" = "$version"; then
+    echo "Puppet version $version already installed."
     exit 0
 fi
 
 echo "Removing any Puppet gem installations ..."
 gem uninstall -a -x puppet facter 2>/dev/null || true
 
-echo "Installing Puppet version $VERSION ..."
-wget -q "https://apt.puppetlabs.com/$PACKAGE"
-dpkg -i "$PACKAGE"
+echo "Installing Puppet version $version ..."
+distro=$(lsb_release -cs)
+pkg="puppetlabs-release-$distro.deb"
+wget -q "https://apt.puppetlabs.com/$pkg"
+dpkg -i "$pkg"
 apt-get update
-apt-get install -y "puppet=$VERSION"
+if test -n "$version"; then
+    pkg="puppet=$version"
+else
+    pkg="puppet"
+fi
+apt-get install -y "$pkg"
