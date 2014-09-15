5aba851c291d1e3bb66056a885ae9ac04a3f89fd
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2014-09-15 20:56:03 +0200

    Auto-detect distro name

--- puppet-installer.sh
+++ puppet-installer.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Helper script to install a specific version of Puppet
-# Usage: puppet-installer.sh <distro name> <Puppet version>
+# Usage: puppet-installer.sh <Puppet version>
 
 set -e
 
-DISTRO=$1
-VERSION=$2
+VERSION=$1
+DISTRO=$(lsb_release -cs)
+PACKAGE="puppetlabs-release-$DISTRO.deb"
 
 CURRENT=$(apt-cache policy puppet | awk '/Installed:/ {print $2}' 2>/dev/null)
 if test "$CURRENT" = "$VERSION"; then
@@ -17,7 +18,7 @@ echo "Removing any Puppet gem installations ..."
 sudo gem uninstall -a -x puppet facter 2>/dev/null || true
 
 echo "Installing Puppet version $VERSION ..."
-wget -q https://apt.puppetlabs.com/puppetlabs-release-$DISTRO.deb
-sudo dpkg -i puppetlabs-release-$DISTRO.deb
+wget -q "https://apt.puppetlabs.com/$PACKAGE"
+sudo dpkg -i "$PACKAGE"
 sudo apt-get update
 sudo apt-get install -y "puppet=$VERSION"
